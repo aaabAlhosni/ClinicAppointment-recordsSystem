bb5249acd9dc4e6f2b184ade9312031642830e8b
aaabAlhosni <binkhalidabdullahv@gmail.com>
2026-06-22 11:06:42 +0400

    Inilized Springboot library.

---
+++ ClinicBookPatientSyetem/src/main/java/Backend/ClinicBookPatientSyetem/ClinicBookPatientSyetemApplication.java
@@ -0,0 +1,13 @@
+package Backend.ClinicBookPatientSyetem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicBookPatientSyetemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicBookPatientSyetemApplication.class, args);
+	}
+
+}
