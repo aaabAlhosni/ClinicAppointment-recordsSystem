11eb76053e1b4b5576eb2cb7e2fb60f3b0c5870d
aaabAlhosni <binkhalidabdullahv@gmail.com>
2026-06-22 15:31:50 +0400

    added Doctor.java for JPA

---
+++ src/main/java/Backend/ClinicBookPatientSyetem/model/entity/Doctor.java
@@ -0,0 +1,33 @@
+package Backend.ClinicBookPatientSyetem.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalTime;
+
+@Entity
+@Table(name = "doctor")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Doctor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "doctor_id")
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "specialty", nullable = false)
+    private String specialty;
+
+    @Column(name = "work_start", nullable = false)
+    private LocalTime workStart;
+
+    @Column(name = "work_end", nullable = false)
+    private LocalTime workEnd;
+}
