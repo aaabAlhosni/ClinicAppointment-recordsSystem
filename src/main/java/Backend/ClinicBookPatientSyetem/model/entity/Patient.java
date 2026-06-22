c952e49b6af2f1797ea38d9048e129d11299a4e7
aaabAlhosni <binkhalidabdullahv@gmail.com>
2026-06-22 15:33:06 +0400

    add Patient.java for JPA.

---
+++ src/main/java/Backend/ClinicBookPatientSyetem/model/entity/Patient.java
@@ -0,0 +1,30 @@
+package Backend.ClinicBookPatientSyetem.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "patient")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Patient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "patient_id")
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "date_of_birth", nullable = false)
+    private LocalDate dateOfBirth;
+
+    @Column(name = "phone", nullable = false)
+    private String phone;
+}
